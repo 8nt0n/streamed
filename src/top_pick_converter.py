b2384ff1b3692797c57255e54343b751ff55ea40
Anton <140619997+8nt0n@users.noreply.github.com>
2026-04-28 16:52:54 +0200

    Delete src directory

--- src/top_pick_converter.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-from collections import Counter
-
-# get most common color in img (add to refresh -> rgb val into data.js pls pls pls)
-img = img.convert("RGB")
-return Counter(img.getdata()).most_common(1)[0][0]
-
